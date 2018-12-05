29ef01e0a02db3043b4412c49b2d3395e63f195e
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2018-12-06 07:28:49 +0800

    pull: Improve semantic; implement check for no release tag case
    Signed-off-by: 林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>

--- snap/local/utilities/parts-hello-override-pull.bash
+++ snap/local/utilities/parts-hello-override-pull.bash
@@ -10,8 +10,9 @@ set \
 
 init(){
 	local \
-		last_committed_tag \
-		last_released_tag \
+		all_upstream_release_tags \
+		last_upstream_release_version \
+		last_snapped_release_version \
 		upstream_version \
 		packaging_revision
 
@@ -23,27 +24,46 @@ init(){
 		git://git.savannah.gnu.org/hello.git \
 		.
 
-	last_committed_tag="$(
+	if \
+		! \
+		git describe \
+			--match 'v*' \
+			--tags \
+			>/dev/null; then
+		printf -- \
+			'Assertion error: No release tags found, cannot determine which revision to build the snap, increase the clone depth!\n' \
+			1>&2
+		exit 1
+	fi
+
+	all_upstream_release_tags="$(
 		git tag \
-			| sed 's/^v//' \
+			--list \
+			'v*'
+	)"
+
+	# We stripped out the prefix 'v' here
+	last_upstream_release_version="$(
+		sed 's/^v//' <<< "${all_upstream_release_tags}" \
 			| sort --version-sort \
 			| tail --lines=1
 	)"
 
-	last_released_tag="$(
+	last_snapped_release_version="$(
 		snap info "${SNAPCRAFT_PROJECT_NAME}" \
 			| awk '$1 == "stable:" { print $2 }' \
 			| cut --delimiter=+ --fields=1
 	)"
 
-	# If the latest tag from the upstream project has not been released to the stable channel, build that tag instead of master.
-	if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
-		git checkout v"${last_committed_tag}"
+	# If the latest tag from the upstream project has not been released to the stable channel, build that tag instead of the development snapshot and publish it in the edge channel.
+	if [ "${last_upstream_release_version}" != "${last_snapped_release_version}" ]; then
+		git checkout v"${last_upstream_release_version}"
 	fi
 
 	unset \
-		last_committed_tag \
-		last_released_tag
+		all_upstream_release_tags \
+		last_upstream_release_version \
+		last_snapped_release_version
 
 	# gnulib submodule has a LOT of history, use the --recommend-shallow feature of git-submodule to avoid pulling all of the history:
 	# DISABLED: Unfortunately the git server git.savannah.gnu.org doesn't allow it: (error: Server does not allow request for unadvertised object)
